4b2582fb61fd85d2079b92e0b2cf31bc018e1519
lobentipan <loben_klien_tipan@dlsu.edu.ph>
2024-02-29 00:09:03 +0800

    various improvements to the website

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const observer = new IntersectionObserver((entries) => {
+const options = {
+    rootMargin: '100px 0px' // Trigger 100px before the element enters the viewport
+  }
+  
+  const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
     });
+  }, options);
+  
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
+  
+const experienceItems = document.querySelectorAll('.experience-item');
+
+experienceItems.forEach(item => {
+  item.addEventListener('mouseenter', () => {
+    item.style.transform = 'scale(1.03)'; /* Slightly scale on hover */
+  });
+
+  item.addEventListener('mouseleave', () => {
+    item.style.transform = 'scale(1)';  /* Reset scale */
+  });
 });
-
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el => observer.observe(el)));
