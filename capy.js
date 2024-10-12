80ad0540ee93a3522277cdfd0e6cb1a7548e5863
lobentipan <loben_klien_tipan@dlsu.edu.ph>
2024-10-12 19:26:30 +0800

    capycapy

---
+++ capy.js
@@ -0,0 +1,4 @@
+// capy.js (currently not needed but can be expanded in the future)
+document.addEventListener("DOMContentLoaded", () => {
+    // Any future animations or functionalities can be added here
+});
